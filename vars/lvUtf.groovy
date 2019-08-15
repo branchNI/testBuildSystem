b492af44b9d396d8744972b13d8de90850557ef6
unknown <brandon.nguyen@ni.com>
2019-08-15 14:38:45 -0500

    Add files

---
+++ vars/lvUtf.groovy
@@ -0,0 +1,11 @@
+def call(lvProjectPath, lvVersion, lvBitness) {
+	def stepsDir = "${WORKSPACE}\\${BUILD_SYSTEM_REPO}\\steps"
+	def reportPath = "${WORKSPACE}\\TEMPDIR\\report.xml"
+	def projectPath = "${WORKSPACE}\\${lvProjectPath}"
+	
+	echo 'Running unit tests on project'
+
+	bat "python -u \"${stepsDir}\\labview_utf.py\" \"${projectPath}\" \"${reportPath}\" ${lvVersion} ${lvBitness}"
+	junit allowEmptyResults: true, testResults: 'TEMPDIR\\report.xml'
+}
+
